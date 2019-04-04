c744d30fb2506e3bcc97dca4bf9bc75ee908f780
Peter Griffin <phirani62@gmail.com>
2019-04-04 08:55:28 +0530

    Day0 Hello World

---
+++ day0.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+
+    string input_string; 
+
+    // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+    getline(cin, input_string); 
+    cout << "Hello, World." << endl;
+    cout << input_string << endl;
+    return 0;
+}
