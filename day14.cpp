4a4070a63e813a2827f716a12e5ab989c6812def
Peter Griffin <phirani62@gmail.com>
2019-04-20 13:59:10 +0530

    Day 13 14 15

---
+++ day14.cpp
@@ -0,0 +1,48 @@
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+
+	Difference(vector<int> elements) {
+        this->elements = elements;
+    }
+
+    void computeDifference() {
+        std::sort(elements.begin(), elements.end());
+        maximumDifference = abs(elements.back() - elements.front());
+    }
+
+};
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
